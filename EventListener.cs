5efcb262190e2892d392045421a7608aa58dde09
Velorexe <rawrexe313@gmail.com>
2018-12-27 12:29:00 +0100

    Pushed the start of the Save Mechanic (Items)

---
+++ EventListener.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class EventListener : MonoBehaviour
+{
+    public PlayerMovement Player;
+    private bool RegisteredEvent = false;
+
+    private void Awake()
+    {
+        GlobalSaveManager.Load();
+        if(null == Player)
+            Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        GlobalSaveManager.RegisterPlayer(GameObject.FindGameObjectWithTag("Player"));
+        List<CustomSaveEvent> Events = GlobalSaveManager.GetRelaventSaveData(SceneManager.GetActiveScene().buildIndex);
+
+        foreach(CustomSaveEvent Event in Events)
+        {
+            var objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == Event.ObjectName);
+            foreach (GameObject saveObject in objects)
+            {
+                if (saveObject.name == Event.ObjectName && saveObject.transform.position == Event.ObjectPosition)
+                {
+                    switch (Event.EventType)
+                    {
+                        case SaveEventType.ITEM:
+                            saveObject.SetActive(false);
+                            break;
+                        case SaveEventType.INTERACTION:
+                            saveObject.SetActive(false);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Player.busyWith != null && RegisteredEvent == false)
+        {
+            //Getting the Object the Player's busy with
+            GameObject eventObject = Player.busyWith;
+
+            //Getting the events from the object
+            CustomEvent interactionEvent = eventObject.GetComponent<CustomEvent>();
+            InteractItem itemEvent = eventObject.GetComponent<InteractItem>();
+
+            if (null != interactionEvent)
+            {
+                RegisteredEvent = true;
+                GlobalSaveManager.RegisterEvent(new CustomSaveEvent(DateTime.Now, SaveEventType.INTERACTION, eventObject, SceneManager.GetActiveScene().buildIndex));
+            }
+            else if (null != itemEvent)
+            {
+                RegisteredEvent = true;
+                GlobalSaveManager.RegisterEvent(new CustomSaveEvent(DateTime.Now, SaveEventType.ITEM, eventObject, SceneManager.GetActiveScene().buildIndex));
+                GlobalSaveManager.Save();
+            }
+        }
+        else if(Player.busyWith == null)
+        {
+            RegisteredEvent = false;
+        }
+    }
+}
---
+++ GlobalSaveManager.cs
@@ -0,0 +1,82 @@
+﻿//Thanks to Omnicron for the reference to
+//https://pastebin.com/YY0XKMCQ
+//For BinarryFormatting any System.Serializable into a custom file
+
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public static class GlobalSaveManager {
+
+    private static GameObject Player;
+    private static List<CustomSaveEvent> EventSaves = new List<CustomSaveEvent>();
+
+    private static EventListener EventListener = new EventListener();
+
+    public static void RegisterPlayer(GameObject player)
+    {
+        Player = player;
+        Debug.Log("Registered Player.");
+    }
+
+    public static void RegisterEvent(CustomSaveEvent customEvent)
+    {
+        EventSaves.Add(customEvent);
+        EventSaves = EventSaves.OrderBy(x => x.EventTime).ToList();
+        Debug.Log(customEvent.ToString());
+    }
+
+    public static List<CustomSaveEvent> GetRelaventSaveData(int sceneIndex)
+    {
+        return EventSaves.Where(x => x.SceneIndex == sceneIndex).ToList();
+    }
+
+    public static void Save()
+    {
+        CustomSaveData DataToSave = new CustomSaveData() { SaveData = EventSaves, PlayerPosition = Player.transform.position, PlayerRotation = Player.transform.rotation};
+
+        GlobalVariables globalVariables = (GlobalVariables)GameObject.Find("Global").GetComponent("GlobalVariables");
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(@"C:\Users\David Blerkselmans\Desktop\PKU TestSaves\Save1.pku", FileMode.OpenOrCreate, FileAccess.Write);
+        bf.Serialize(file, DataToSave);
+        file.Close();
+    }
+
+    public static void Load()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(@"C:\Users\David Blerkselmans\Desktop\PKU TestSaves\Save1.pku", FileMode.Open, FileAccess.Read);
+        CustomSaveData DataToLoad = (CustomSaveData)bf.Deserialize(file);
+
+        EventSaves = DataToLoad.SaveData;
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
+        Player.transform.position = DataToLoad.PlayerPosition;
+        Player.transform.rotation = DataToLoad.PlayerRotation;
+
+        EventSaves = EventSaves.OrderBy(x => x.EventTime).ToList();
+    }
+
+    [System.Serializable]
+    private class CustomSaveData
+    {
+        public SerializableVector3 PlayerPosition;
+        public SerializableQuaternion PlayerRotation;
+
+        //public Pokemon[] Party;
+        //public Bag PlayerBag;
+
+        public List<CustomSaveEvent> SaveData;
+    }
+}
+
+[System.Serializable]
+public enum SaveEventType
+{
+    ITEM,
+    BATTLE,
+    INTERACTION,
+    UNKNOWN
+}
